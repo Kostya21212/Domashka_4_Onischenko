c0e07546938d582a2a60d57b5988128158c6b7de
Kostyantyn Onischenko <Onischenko411@gmail.com>
2023-11-21 15:27:28 +0200

    1 file added

---
+++ app.js
@@ -0,0 +1,23 @@
+// // 3 домашка////
+// const user = prompt(`Як вас звуть? `)
+// const age = prompt( `Ваш вік?`)
+// const city = prompt(`Місто де ви проживаєте?`)
+// const four = user + age + city
+// console.log(`${four}`)
+
+
+// 4 домашка///
+const firstNumber = +prompt(`Додайте перше число`)
+const secondNumber = +prompt(`Додайте друге число`)
+
+
+alert(`Користувач ввів ${firstNumber} і ${secondNumber}:
+${firstNumber} + ${secondNumber}= ${firstNumber + secondNumber}
+${firstNumber} - ${secondNumber}= ${firstNumber - secondNumber}
+${firstNumber} * ${secondNumber}= ${firstNumber * secondNumber}
+${firstNumber} / ${secondNumber}= ${firstNumber / secondNumber}
+`)
+
+
+
+
